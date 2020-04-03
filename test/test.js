c7f63b672bae809c9c76faeb79b8ddfecf1d88f5
Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2020-04-03 00:36:50 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,5 @@
 const snoowrap = require("snoowrap");
 const creds = require("../oauth_info.json");
-
-// const creds = {
-//     "client_id": "7kamBn3K5RlZqg",
-//     "client_secret":"Z3K07R_o1IvpsPLdhRDGkt-K6tU",
-//     "user_agent": "A bot for finding the latest sneaker deals",
-//     "refresh_token" : "41811051-sstmw1mb4nWOmarX_YnlZx6VltQ",
-//     "access_token" : "41811051-11SatmLC2waVMqDVklRSje59R5o"
-// }
   
 const Reddit = new snoowrap({
   userAgent: creds.user_agent,
